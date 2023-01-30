998c5a86bca7de0a7f86775c135e129a09eea927
SelimImranov <114604872+SelimImranov@users.noreply.github.com>
2023-01-30 18:34:01 +0400

    Add files via upload

---
+++ lesson33/main.js
@@ -0,0 +1,95 @@
+// var adaminyas = prompt("YASINIZI QEYD EDIN")
+// console.log(adaminyas)
+// if (adaminyas <= 30) {
+//   document.write(adaminyas ** 2)
+// } else if (adaminyas > 30 && adaminyas <= 40) {
+//   document.write("LLL")
+// } else {
+//   document.write("sehv melumat qeyd elemisiniz")
+// var adaminyas= prompt("YASINIZI QEYD ELIYIN")
+// console.log(adaminyas)
+// if(adaminyas<=30){
+//     document.write(adaminyas**2)
+// } else if (adaminyas >30 && adaminyas<=40){
+//     document.write("lll")
+// }else{
+//     document.write("sehv melumatdir")
+// }
+// }
+// var x="546"
+// for ( var i=0; i<x.length; i++){
+//   if(i==x.length-1){
+//     console.log(x[i])
+
+//   }
+ 
+// }
+
+// var word="selim";
+// var netice="";
+// for(var x=word.length-1; x>=0; --x){
+//  console.log(word[x])
+// }
+// if(word==netice)
+// var eded="5"
+// for (var z = 0; z <=9; z++) {
+//  console.log(z*eded)
+  
+// }
+// var z="SALAM"
+// z.slice(2,4) 
+
+// // la cixir
+
+//  var password="Salam"
+//  for(var i=0; i<=password.length; i++){
+//      if(password. charCodeAt(i)>=65 && password.charCodeAt(i)<=90){
+//      console.log(password[i]+ "boyuk herif var")
+//      }
+//  } 
+
+// function mathmueltiple(number1, number2){
+//     console.log(number1*number2)
+// }
+// mathmueltiple(15,9)
+// mathmueltiple(8,9)
+// mathmueltiple(4,2)
+
+// function vurma(eded){
+//     for(var i=1; i<=9;i++){
+//         console.log(`${i} * ${eded} = ${i*eded}`)
+//     }
+// }
+// vurma(5)
+// vurma(8)
+// vurma(9)
+// "vurma cedveli "
+
+// function vurma(eded){
+//         for(var i=1; i<=9; i++){
+//          console.log(`${i} * ${eded} = ${ i* eded} `)
+//     }
+//  }
+//   vurma(1)
+//   vurma(2)
+//   vurma(3)
+//   vurma(4) 
+//   vurma(5)
+//   vurma(6)
+//   vurma(7)
+//   vurma(8)
+//   vurma(9)
+//   vurma(10)
+
+
+
+
+
+
+function vurma(eded){
+    for (var i=1; i<=9; i++){
+        console.log(`${i} * ${eded} = ${i*eded}`)
+    }
+}
+vurma(1)
+vurma(2)
