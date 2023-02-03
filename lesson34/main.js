a13ab7f66a32947cbf6826bf941dc1fea7194f5a
SelimImranov <114604872+SelimImranov@users.noreply.github.com>
2023-02-03 18:54:10 +0400

    Add files via upload

---
+++ lesson34/main.js
@@ -0,0 +1,19 @@
+// var arr = [9,3,14,5,7,65,97,5,82,101,5,73,82,5,101,75,99,80 ]
+// var tekrar=[101]
+// var enboyukelement = arr[0]
+
+// for (var x = 0; x <= arr.length; x++) {
+//     if (enboyukelement < arr[x]) {
+//         enboyukelement = arr[x]
+//         console.log(tekrar+arr[9])
+//     }
+// }
+// console.log(enboyukelement)
+
+// var i;
+// var fib = [0, 1]; 
+// for (i = 2; i <= 10; i++) {
+  
+//   fib[i] = fib[i - 2] + fib[i - 1];
+//   console.log(fib[i]);
+// }
