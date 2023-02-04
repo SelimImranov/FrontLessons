e3617f4d89c2006242850935506b21cceb84b566
SelimImranov <114604872+SelimImranov@users.noreply.github.com>
2023-02-04 12:05:45 +0400

    Add files via upload

--- lesson34/main.js
+++ lesson34/main.js
@@ -1,19 +1,38 @@
-// var arr = [9,3,14,5,7,65,97,5,82,101,5,73,82,5,101,75,99,80 ]
-// var tekrar=[101]
-// var enboyukelement = arr[0]
+// 1
+//  var data="SELIM,IMRANOV,SAHIN"
+//  var data= data.split()
 
-// for (var x = 0; x <= arr.length; x++) {
-//     if (enboyukelement < arr[x]) {
-//         enboyukelement = arr[x]
-//         console.log(tekrar+arr[9])
+
+
+//  3
+// var array=[9,3,14,5,7,65,97,5,82,101,5,73,82,5,101,75,99,80 ]
+// var count=0;
+// for (var item of array) {
+//     if(item==5){
+//         count++
 //     }
-// }
-// console.log(enboyukelement)
 
-// var i;
-// var fib = [0, 1]; 
-// for (i = 2; i <= 10; i++) {
-  
-//   fib[i] = fib[i - 2] + fib[i - 1];
-//   console.log(fib[i]);
 // }
+
+
+// 4
+// var array=[9,3,14,5,7,65,97,5,82,101,5,73,82,5,101,75,99,80 ]
+// array.sort(function(a, b){return a - b});
+
+
+// 5
+//  var arr=[9,3,14,5,7,65,97,5,82,101,5,73,82,5,101,75,99,80 ]
+//  var enboyukelemnt=[0]
+//  for(var i=0; i<arr.length;i++){
+//     if(enboyukelemnt<arr[i]){
+//         enboyukelemnt=arr[i]
+//     }
+//  }
+//  console.log(enboyukelemnt)
+
+
+// 6
+
+// var fullname="Selim Imranov"
+// fullname[0]
+// fullname[6]
