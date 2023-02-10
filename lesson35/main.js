e413c5113d57c37d0dfff943337578f32be9b3dc
SelimImranov <114604872+SelimImranov@users.noreply.github.com>
2023-02-10 19:54:02 +0400

    Add files via upload

---
+++ lesson35/main.js
@@ -0,0 +1,114 @@
+//  var adaminyas = prompt("YASINIZI QEYD EDIN")
+//  console.log(adaminyas)
+//  if (adaminyas <= 30) {
+//    document.write(adaminyas ** 2)
+//  } else if (adaminyas > 30 && adaminyas <=40){
+
+//  } else {
+//    document.write("sehv melumat qeyd elemisiniz")
+
+//  }
+
+// 1
+//  var arr2 = [
+//   {    name:'test',    key:1  },
+//   {    name:'task',    key:2  },
+//   {    name:'tanqo',    key:3  },
+//   {    name:'like',    key:4  },
+//   {    name:'task',    key:5  },
+//   {    name:'trust',    key:6  },
+//   {    name:'test',    key:7  },
+//   {    name:'last',    key:8  },
+//   {    name:'tanqo',    key:9  },
+//   {    name:'elephant',    key:10  },
+//   {    name:'love',    key:11  },
+//   {    name:'small',    key:12  },
+//   {    name:'little',    key:13  },
+// ]
+
+// var newarr=[]
+// for(var item of arr2){
+//   if(item.name[0]=="t" ){
+//     newarr.push(item)
+//     console.log(item.name)
+//   }
+// }
+
+// 2
+// var arr2 = [
+//   { name: 'test', key: 1 },
+//   { name: 'task', key: 2 },
+//   { name: 'tanqo', key: 3 },
+//   { name: 'like', key: 4 },
+//   { name: 'task', key: 5 },
+//   { name: 'trust', key: 6 },
+//   { name: 'test', key: 7 },
+//   { name: 'last', key: 8 },
+//   { name: 'tanqo', key: 9 },
+//   { name: 'elephant', key: 10 },
+//   { name: 'love', key: 11 },
+//   { name: 'small', key: 12 },
+//   { name: 'little', key: 13 },
+// ]
+// var newarr = []
+//  for (var item of arr2) {
+//   if(item.name[item.name.length-1]=="t"){
+//      console.log (item.name)
+//      newarr.push(item)
+//    }
+//   }
+
+//    for(var i=0; i.arr2.key.lenght;i++){
+//     cem=cem+arr2.key[i]
+//     newarr.push(arr2.key)
+//    }
+
+  
+//   console.log(cem)
+// 3
+// var arr2 = [
+//   {    name:'test',    key:1  },
+//   {    name:'task',    key:2  },
+//   {    name:'tanqo',    key:3  },
+//   {    name:'like',    key:4  },
+//   {    name:'task',    key:5  },
+//   {    name:'trust',    key:6  },
+//   {    name:'test',    key:7  },
+//   {    name:'last',    key:8  },
+//   {    name:'tanqo',    key:9  },
+//   {    name:'elephant',    key:10  },
+//   {    name:'love',    key:11  },
+//   {    name:'small',    key:12  },
+//   {    name:'little',    key:13  },
+// ]
+// var newarr=[]
+// for (var item of arr2) {
+//   if(item.name[item.name.length-1]=="e"){
+//      console.log (item.name)
+//     item[0]="superdew"
+  //   newarr.push(item)
+  //  }
+  // }
+  var longWords = [
+    {    name:'test',    key:1  },
+    {    name:'task',    key:2  },
+    {    name:'tanqo',    key:3  },
+    {    name:'like',    key:4  },
+    {    name:'task',    key:5  },
+    {    name:'trust',    key:6  },
+    {    name:'test',    key:7  },
+    {    name:'last',    key:8  },
+    {    name:'tanqo',    key:9  },
+    {    name:'elephant',    key:10  },
+    {    name:'love',    key:11  },
+    {    name:'small',    key:12  },
+    {    name:'little',    key:13  },
+]
+  var longest = 0;
+  for (var i = 0; i < longWords.name.length; i++) {
+  if (longest < longWords[i].name.length) {
+      longest = longWords[i].name;
+      longest.push(arr2)
+    }
+  }
+console.log(longest)
