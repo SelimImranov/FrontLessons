6357e1e0695a5db5d35ab9420040506c12a40654
SelimImranov <114604872+SelimImranov@users.noreply.github.com>
2023-02-20 19:59:03 +0400

    Add files via upload

---
+++ lesson37/test.js
@@ -0,0 +1,93 @@
+// var word = "ey babek kebab y !"
+// var netice = ""
+// for (var x = word.length - 1; x >= 0; x--) {
+//   netice+=word[x]
+
+
+// }
+// console.log(netice)
+
+// var arr2= [3, 2, 21, 12, 4, 12, 4, 23, 3, 23, 5, 34, 35, 3, 4, 234, 2, 105, 24, 2, 342, 34, 140, 34, 23, 175, 342312, 1, 2, 1, 70, 21, 24, 23, 24, 23, 24, 2, 24, 34]
+// for (var i = 0; i < arr2.length; i++) {
+//     if (i % 7 == 0 && i % 5 == 0) {
+//        console.log(arr[i])
+//     }
+// }
+
+
+
+
+
+
+//  let SET = 'SET_DATA';
+//  let GET = 'GET_DATA'
+//  let DELETE = 'DELETE_DATA'
+//  function parametr(type){
+//      switch(type){
+//          case "SET":
+//          console.log("SET_DATA")
+//          for(var i=0;i<=10;i++){
+//             if(i==4){
+//                 console.log(i+"setdata")
+//             }
+//          }
+//          break;
+//          case "GET":
+//          console.log("GET_DATA")
+//          break;
+//          case "delete":
+//          console.log("DELETE_DATA")
+//          break;
+//          default:
+//              console.log("bele bir emeliyet yoxdur")
+//      }
+//  }
+//  parametr("SET")
+//  parametr("GET")
+//  parametr("delete")
+
+// let mL = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+// let day;
+// function parametr(type) {
+//     switch (type) {
+//         case "January":
+//             console.log(31 + "January")
+//             break;
+//         case "February":
+//             console.log(28, 29 + "February")
+//             break;
+//         case "March":
+//             console.log(31 + "March")
+//             break;
+//         case "April":
+//             console.log(30 + "April")
+//             break;
+
+//         case "May":
+//             console.log(31 + "May")
+//             break;
+
+
+//         case "June":
+//             console.log(30+"June")
+//             break;
+//         case "August":
+//             console.log(31+"August")
+//             break;
+//         case "Semtemper":
+//             console.log(30+"Semtemper")
+//             break;
+//         case "Octomper":
+//             console.log(31+"Octomper")
+//             break;
+//         case "November":
+//             console.log(31+"November")
+//             break;
+//         case "December":
+//             console.log(31+"December")
+//             break;
+//     }
+
+// }
+// parametr("January")
+// parametr("February")
