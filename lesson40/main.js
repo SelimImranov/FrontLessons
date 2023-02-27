ef5f57c6e19f842b5d5d31629b7636b328148cd5
SelimImranov <114604872+SelimImranov@users.noreply.github.com>
2023-02-27 19:25:15 +0400

    Add files via upload

---
+++ lesson40/main.js
@@ -0,0 +1,23 @@
+// var ul = document.createElement("ul")
+// // function todolist() {
+// //     var input = document.getElementById("textinput").value
+// //     var li = document.createElement("li")
+// //     li.innerHTML = input
+// //     li.classList.add("list-group-item")
+    
+// //     ul.classList.add("list-group")
+// //     ul.classList.add("container")
+// //     ul.appendChild(li)
+// //     var input = document.getElementById("textinput").value=" "
+// // }
+// // document.body.appendChild(ul)
+// function test(sssss){
+ 
+    
+//     console.log(sssss.parentElement.parentElement.firstElementChild)
+//     console.log(sssss.nextElementSibling)
+//     var img=sssss.parentElement.parentElement.firstElementChild
+//     img.style.filter="blur(0px)"
+//     var qaqzanilan=sssss.nextElementSibling
+//     qaqzanilan.classList.remove("d-none")
+// }
